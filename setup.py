2e2bce688043e4867ab440dc91cb080a69fcf414
Michaël Piron <4469877+michaelpiron@users.noreply.github.com>
2023-05-03 20:36:07 +0200

    Revert "Bump to v0.0.8"

--- setup.py
+++ setup.py
@@ -10,13 +10,11 @@ extras_require = {
 
 setuptools.setup(
     name="eltakobus",
-    version="0.0.8",
+    version="0.0.7",
     author="chrysn",
     author_email="chrysn@fsfe.org",
-    maintainer="michaelpiron",
-    maintainer_email="code@michaelpiron.be",
     description="Library for participating in the Eltako Series 14 RS485 bus",
-    url="https://github.com/michaelpiron/eltako14bus",
+    url="https://gitlab.com/chrysn/eltakobus",
     packages=setuptools.find_packages(),
     extras_require=extras_require,
     # Not that there'd be tests, but at least it fetches the right dependencies and syntax checks everything
